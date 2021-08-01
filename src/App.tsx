9c786a5df2bb73146ef0c6093099864bb18b8933
mitrich83 <dmitry.v.malykhin@gmail.com>
2021-08-01 21:17:13 +0300

    3 version , one window

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
+import {Route, NavLink, useHistory} from 'react-router-dom';
 import './App.css'
 import {Display} from './components/Display/Display';
 import {Button} from './components/Button/Button';
@@ -14,9 +15,6 @@ const App = () => {
     const [minValue, setMinValue] = useState<number>(0)
 
     const [value, setValue] = useState<number | string>(0)
-
-   // const [error, setError] =useState<string>('')
-
     const [isDisabled, setIsDisabled] = useState<boolean>(false)
 
 
@@ -24,24 +22,25 @@ const App = () => {
         if (value > maxValue) return;
         else (setValue(+value + 1))
     }
-
-    const resetNumber = () => { setValue(minValue)}
-
+    const resetNumber = () => {
+        setValue(minValue)
+    }
     const onChangeMaxHandler = (e: ChangeEvent<HTMLInputElement>) => {
         localStorage.setItem(
             'maxValue', JSON.stringify(e.currentTarget.value))
         setMaxValue(+e.currentTarget.value)
     }
-
     const onChangeMinHandler = (e: ChangeEvent<HTMLInputElement>) => {
         localStorage.setItem(
             'minValue', JSON.stringify(e.currentTarget.value))
         setMinValue(+e.currentTarget.value)
     }
+    const setButtonNumber = () => {
+        setValue(minValue)
+    }
 
     useEffect(() => {
         const minValue = localStorage.getItem('minValue');
-
         const maxValue = localStorage.getItem('maxValue');
 
         if (minValue && maxValue) {
@@ -49,7 +48,6 @@ const App = () => {
             setMaxValue(+maxValue)
         }
     }, [])
-
     useEffect(() => {
         if (maxValue < minValue ||
             minValue < 0 ||
@@ -63,49 +61,53 @@ const App = () => {
         setIsDisabled(false)
     }, [minValue, maxValue])
 
-    const setButtonNumber = () => {
-        setValue(minValue)
-        setIsDisabled(true)
-    }
-
     return (
         <div className={'counter'}>
-            <div className={'showNumbers'}>
-                <div className={'display'}>
-                    <Display value={value}
-                             maxValue={maxValue}
-                            // error={error}
-                    />
-                </div>
-                <div>
-                    <Button title={'Inc'}
-                            callback={incNumber}
-                            value={value}
-                            isDisabled={value === maxValue}
-                    />
-                    <Button title={'Reset'}
-                            callback={resetNumber}
-                            value={value}
-                            isDisabled={value === minValue}
-                    />
-                </div>
+            <div className={'showDisplay'}>
+                <Route path={'/display'}>
+                    <div className={'display'}>
+                        <Display value={value} maxValue={maxValue}/>
+                    </div>
+                    <div className={'buttonsDisplay'}>
+                        <Button title={'Inc'}
+                                callback={incNumber}
+                                value={value}
+                                isDisabled={value === maxValue}
+                        />
+                        <Button title={'Reset'}
+                                callback={resetNumber}
+                                value={value}
+                                isDisabled={value === minValue}
+                        />
+                        <NavLink to={'/setting'}>
+                            <Button title={'Set'}
+                                    callback={setButtonNumber}
+                                    value={value}
+                            />
+                        </NavLink>
+                    </div>
+                </Route>
             </div>
             <div className={'showSettings'}>
-                <div className={'setting'}>
-                    <Setting maxValue={maxValue}
-                             minValue={minValue}
-                             onChangeMaxHandler={onChangeMaxHandler}
-                             onChangeMinHandler={onChangeMinHandler}
-                    />
-                </div>
-                <div>
-                    <Button
-                        title={'Set'}
-                        value={value}
-                        callback={setButtonNumber}
-                        isDisabled={isDisabled}
-                    />
-                </div>
+                <Route path={'/setting'}>
+                    <div className={'setting'}>
+                        <Setting maxValue={maxValue}
+                                 minValue={minValue}
+                                 onChangeMaxHandler={onChangeMaxHandler}
+                                 onChangeMinHandler={onChangeMinHandler}
+                        />
+                    </div>
+                    <div className={'buttonSetting'}>
+                        <NavLink to={'/display'}>
+                            <Button
+                                title={'Set'}
+                                value={value}
+                                callback={setButtonNumber}
+                                isDisabled={isDisabled}
+                            />
+                        </NavLink>
+                    </div>
+                </Route>
             </div>
         </div>
     )
--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import s from './Display.module.css'
 
-
 type DisplayType = {
     value: number | string
     maxValue: number
-   // error:string
 }
 
 export const Display = (props: DisplayType) => {
@@ -13,7 +11,6 @@ export const Display = (props: DisplayType) => {
     return (
         <div className={props.value === props.maxValue ? s.limit : ''}>
             {props.value}
-            {/*{props.value ? props.value :props.error}*/}
         </div>
     )
 }
